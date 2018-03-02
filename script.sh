754120312a32ffd4f0b4735e76be7ca2bd234cdf
nealcaffrey135 <nealcaffrey135@gmail.com>
2018-03-02 14:07:22 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+echo " Openstack Installation Script [ ubuntu VM] By Manoj Choudhary C"
+echo " Basic Requirements : 2 Bridge Network interface , virtualization switched on , Number of cores per processor = 3  "
