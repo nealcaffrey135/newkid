e2e3dbecb8b1962750e2199c310b053adaefdbd0
nealcaffrey135 <nealcaffrey135@gmail.com>
2018-03-02 21:46:38 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-
-echo " Openstack Installation Script [ ubuntu VM] By Manoj Choudhary C"
-echo " Basic Requirements : 2 Bridge Network interface , virtualization switched on , Number of cores per processor = 3  "
